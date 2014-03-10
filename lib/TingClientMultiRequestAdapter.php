3f9834ce1d3fa39efecf900b9d34310848aa29c3
Arni <al@randersbib.dk>
2014-03-10 10:41:56 +0100

    Adde row count and raw search count

--- lib/TingClientMultiRequestAdapter.php
+++ lib/TingClientMultiRequestAdapter.php
@@ -149,7 +149,7 @@ class TingClientMultiRequestAdapter extends TingClientRequestAdapter {
      // CURLOPT_URL => $url,
       CURLOPT_USERAGENT => $agent,
       CURLOPT_RETURNTRANSFER => TRUE,
-      CURLOPT_PROXY => '172.18.0.40:8080' //Todo remove developercode
+      CURLOPT_PROXY => '172.18.0.30:8080' //Todo remove developercode
     );
 
     if ($method == 'POST') {
